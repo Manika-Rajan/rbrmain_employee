bfe905080905d925b6c9934f111291253001a04e
Manika-Rajan <105493699+Manika-Rajan@users.noreply.github.com>
2026-06-17 14:44:43 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4273,7 +4273,7 @@ export default function App() {
               </div>
             ) : null}
 
-            {!isCatalog && !isSales && !isInstantAdmin ? (
+            {{!isCatalog && !isSales && !isAdsIntelligence && !isInstantAdmin ? ( ? (
               <>
                 <div className="compareHeader">
                   <div className="compareTitleRow">
